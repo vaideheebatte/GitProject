d4f2f3ed6053212f03515106041517010ce150ed
vaideheebatte <battevaidehee2001@gmail.com>
2022-09-12 12:29:59 +0530

    Change code

--- src/com/git/GitPractice.java
+++ src/com/git/GitPractice.java
@@ -4,7 +4,9 @@ public class GitPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello");
+		System.out.println("World");
+		
 	}
 
 }
